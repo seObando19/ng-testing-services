5ff4f7ffcd3e266c0161e99f1df46420c06b7be8
Sebastian Obando <sebastiano-10@hotmail.com>
2023-02-20 10:44:38 -0500

    agregando las configuraciones respectivas para trabajar en pruebas unitarias, la configuracion de mocha y demas

--- karma.conf.js
+++ karma.conf.js
@@ -48,6 +48,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
+    customLauchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
